4ef73e083005185d586fb75ae0aad39eb60be577
harshavardhan2112 <108981384+harshavardhan2112@users.noreply.github.com>
2022-07-09 16:48:01 +0530

    admin

---
+++ about.php
@@ -0,0 +1,16 @@
+        <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end mb-4" style="background: #ffffff;">
+                    	 <h1 class="text-uppercase text-black font-italic">About</h1>
+                        <hr class="divider my-4" />
+                    </div>
+                    
+                </div>
+            </div>
+
+    <section class="page-section">
+        <div class="container">
+    <?php echo html_entity_decode($_SESSION['system']['about_content']) ?>        
+            
+        </div>
+        </section>
---
+++ header.php
@@ -0,0 +1,22 @@
+ 		
+        <title><?php echo $_SESSION['system']['name'] ?></title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        
+        
+        <link href="admin/assets/css/jquery.datetimepicker.min.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+        
+        <link href="admin/assets/vendor/bootstrap-datepicker/css/bootstrap-datepicker.css" rel="stylesheet" />
+        <link href="css/styles.css" rel="stylesheet" />
+        <link type="text/css" rel="stylesheet" href="admin/assets/css/jquery-te-1.4.0.css">
+
+         <link href="admin/assets/css/select2.min.css" rel="stylesheet">
+
+        <script src="admin/assets/vendor/jquery/jquery.min.js"></script>
+        <script src="admin/assets/vendor/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
+
+    
+
+
+
